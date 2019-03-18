36630149c6a32df94ea70742b35e5065611acdf5
kokushkin <fipsiksi@yandex.ru>
2019-03-18 13:11:58 +0200

    before 10.2

---
+++ ACControllerMine/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+
+namespace ACControllerMine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo
+            .CreateSpecificCulture("es-MX");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+
+            var resMan = new ResourceManager(
+            "ACControllerMine.strings",
+            typeof(Program).GetTypeInfo().Assembly);
+            Console.WriteLine(
+            resMan.GetString("ExhaustAirTemp") +
+            TempControl.ExhaustAirTemp);
+        }
+    }
+}
---
+++ ACControllerMine/TempControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACControllerMine
+{
+    public class TempControl
+    {
+        public static double ExhaustAirTemp { get; internal set; }
+        public static double CoolantTemp { get; internal set; }
+        public static double OutsideAirTemp { get; internal set; }
+    }
+}
