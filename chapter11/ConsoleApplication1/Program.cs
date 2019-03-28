eb8857d06ca99665035e9b63a1ab4b8378f32068
kokushkin <fipsiksi@yandex.ru>
2019-03-29 00:31:09 +0200

    before 11.3

---
+++ chapter11/ConsoleApplication1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://www.bing.com";
+            var firstRequest = MeasureRequest(uri);
+            var secondRequest = MeasureRequest(uri);
+            if (firstRequest.Item1 != HttpStatusCode.OK &&
+            secondRequest.Item1 != HttpStatusCode.OK)
+            {
+                Console.WriteLine("Unexpected status code");
+            }
+            else
+            {
+                Console.WriteLine($"First request took {firstRequest.Item2}ms");
+                Console.WriteLine($"Second request took {secondRequest.Item2}ms");
+            }
+            Console.ReadLine();
+        }
+
+        static HttpClient client = new HttpClient();
+
+        static Tuple<HttpStatusCode, long> MeasureRequest(string uri)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var response = client.GetAsync(uri).Result;
+            response.Content.ReadAsStringAsync().Wait();
+            stopwatch.Stop();
+
+            return new Tuple<HttpStatusCode, long>(
+                response.StatusCode,
+                stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
